c30b57e3dfaf93622ae641e2f1bae6925c4a674f
Nathanael Burt <nathanael.burt@gmail.com>
2014-04-24 15:28:09 -0600

    can update cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -9,12 +9,30 @@ class CatsController < ApplicationController
   end
 
   def create
-    @cat =Cat.new(allowed_paramaters)
+    @cat = Cat.new(allowed_paramaters)
 
     if @cat.save
       redirect_to cats_path
     else
       render :new
+    end
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.update(params[:id], allowed_paramaters)
+
+    if @cat.save
+      redirect_to cat_path
+    else
+      render :edit
     end
   end
 
--- spec/features/cats_spec.rb
+++ spec/features/cats_spec.rb
@@ -2,24 +2,40 @@ require 'spec_helper'
 
 feature 'it manages cats' do
 
-  scenario 'can create cats' do
+  before do
     visit '/'
     click_link 'Cats'
     click_link 'Create Cat'
+  end
+
+  scenario 'can create cats' do
     fill_in 'cat[name]', :with => 'Mr. Kitty'
     fill_in 'cat[color]', :with => 'Black'
-    click_button 'Create Cat'
+    click_button 'Create Kitty'
     expect(page).to have_content 'Mr. Kitty'
     expect(page).to have_content 'Black'
   end
 
   scenario 'returns error if don\'t fill in fields' do
-    visit '/'
-    click_link 'Cats'
-    click_link 'Create Cat'
-    click_button 'Create Cat'
+    click_button 'Create Kitty'
     expect(page).to have_content 'Name can\'t be blank'
     expect(page).to have_content 'Color can\'t be blank'
   end
 
+  scenario 'can update cats' do
+    fill_in 'cat[name]', :with => 'Mr. Kitty'
+    fill_in 'cat[color]', :with => 'Black'
+    click_button 'Create Kitty'
+
+    click_link 'Mr. Kitty'
+    expect(page).to have_content 'Mr. Kitty'
+    expect(page).to have_content 'Black'
+    click_link 'Edit Mr. Kitty'
+
+    fill_in 'cat[name]', :with => 'Mrs. Kitty'
+    fill_in 'cat[color]', :with => 'White'
+    click_button 'Edit Kitty'
+    expect(page).to have_content 'Mrs. Kitty'
+    expect(page).to have_content 'White'
+  end
 end
